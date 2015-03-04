a96e0bc455c7b32ef539c4bc0343aa8d077374cc
Alex Memering <memeriaj@rose-hulman.edu>
2015-03-03 23:41:21 -0500

    Added fillup section
    Added more inputs if the stop is a fillup.  Also moved the inputs into
    their own control.

--- src/main.js
+++ src/main.js
@@ -2,11 +2,31 @@ angular.module('main', ['firebase', 'ngRoute'])
 
   .value('fbURL', 'https://carlog-inferno.firebaseio.com/')
 
-  .factory('firebaseData', function($firebase, fbURL){
-    return $firebase(new Firebase(fbURL)).$asArray();
+  .factory('fbTop', function($firebase, fbURL){
+    return $firebase(new Firebase(fbURL)).$asObject();
   })
 
-  .controller('cntrl', ['$scope', 'firebaseData', function($scope, firebaseData){
+  .controller('MainCntrl', ['$scope', 'fbTop', function($scope, fbTop){
     $scope.data = 'Here some data';
-    $scope.fbData = firebaseData;
+    $scope.fbData = fbTop;
+
+
   }])
+
+  .controller('EntryCntrl', ['$scope', '$firebase', 'fbTop', function($scope, $firebase, fbTop){
+    $scope.addLog = function(){
+      var fbEntries = $firebase(fbTop.$ref().child('entries')).$asArray();
+      $scope.subGroup.time = Date.now();
+      var addedRef = fbEntries.$add($scope.subGroup);
+      $scope.subGroup.submitted = 'Traveled to '+$scope.subGroup.loc+
+        ' ending at mile '+$scope.subGroup.mile+' at an average MPG of '+
+        $scope.subGroup.mpg+'.';
+      $scope.subGroup.location = null;
+      $scope.subGroup.mpg = null;
+      $scope.subGroup.mileage = null;
+    };
+    $scope.subGroup = {};
+    $scope.subGroup.fillup = false;
+    $scope.fuelup = {};
+    $scope.subGroup.submitted = 'Nothing submitted';
+  }]);
