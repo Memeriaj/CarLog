1504b58c6637c2b80b0c9b0211c2d2ddf0ccdcba
Alex Memering <memeriaj@rose-hulman.edu>
2015-03-04 04:22:53 -0500

    Logs have an actual table

--- src/Logs/logs.js
+++ src/Logs/logs.js
@@ -1,4 +1,10 @@
 angular.module('main')
-  .controller('LogsCntrl', ['$scope', 'fbTop', function($scope, fbTop){
+  .controller('LogsCntrl', ['$scope', '$firebase', 'fbTop', function($scope, $firebase, fbTop){
     $scope.data = 'Here some data';
+    $scope.fbEntries = $firebase(fbTop.$inst().$ref().child('entries')).$asArray();
+    console.log($scope.fbEntries);
+    $scope.getDate = function(time){
+      var d = new Date(time);
+      return d.toGMTString();
+    };
   }]);
