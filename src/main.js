8bfcff194c5f0373a2b323976e2513c0fb3b060b
Alex Memering <memeriaj@rose-hulman.edu>
2015-03-04 00:46:40 -0500

    Moved entry controller to new file

--- src/main.js
+++ src/main.js
@@ -20,30 +20,4 @@ angular.module('main', ['firebase', 'ngRoute'])
       .otherwise({
         redirectTo:'/entry'
       });
-  }])
-
-  .controller('EntryCntrl', ['$scope', '$firebase', 'fbTop', function($scope, $firebase, fbTop){
-    var addAnEntry = function(fillupId){
-      var fbEntries = $firebase(fbTop.$inst().$ref().child('entries')).$asArray();
-      $scope.subGroup.time = Date.now();
-      $scope.subGroup.fillup = fillupId;
-      fbEntries.$add($scope.subGroup);
-      $scope.subGroup.location = null;
-      $scope.subGroup.mpg = null;
-      $scope.subGroup.mileage = null;
-      $scope.subGroup.fillup = false;
-    };
-    $scope.addLog = function(){
-      if($scope.subGroup.fillup){
-        var fbFillups = $firebase(fbTop.$inst().$ref().child('fillups')).$asArray();
-        fbFillups.$add($scope.fuelup).then(function(ref){
-          addAnEntry(ref.key());
-        });
-      } else{
-        addAnEntry(false);
-      }
-    };
-    $scope.subGroup = {};
-    $scope.subGroup.fillup = false;
-    $scope.fuelup = {};
   }]);
