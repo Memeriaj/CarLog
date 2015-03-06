7f85223c12f9e4bd18268ae8e68dc8037fd5c5d2
Jordan Moore <moorejm@rose-hulman.edu>
2015-03-06 05:30:01 -0500

    Most docs I see use path.join(), also added port env variable

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var express = require('express');
+var path = require('path');
+
 var app = express();
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.listen(8080);
-console.log('Started');
+// Use the environment variable if set, otherwise 8080
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+	console.log('Started on port ' + port);
+});
